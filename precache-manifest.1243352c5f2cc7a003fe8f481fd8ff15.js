374789569f3df150702e8809e7f1e9d654c7fc2b
Serhii <vasylyk29@gmail.com>
2020-08-18 17:27:40 +0300

    Updates

---
+++ precache-manifest.1243352c5f2cc7a003fe8f481fd8ff15.js
@@ -0,0 +1,186 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87993dd0bdfa7a88120992ea5a730966",
+    "url": "/devima_react-test/index.html"
+  },
+  {
+    "revision": "1f68bf763b207bfda9e5",
+    "url": "/devima_react-test/static/css/main.8e949a62.chunk.css"
+  },
+  {
+    "revision": "a2d6643c5f4cae987f92",
+    "url": "/devima_react-test/static/js/2.8f7ff79b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/devima_react-test/static/js/2.8f7ff79b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "1f68bf763b207bfda9e5",
+    "url": "/devima_react-test/static/js/main.67a1f068.chunk.js"
+  },
+  {
+    "revision": "165551a867d74bcab723",
+    "url": "/devima_react-test/static/js/runtime-main.c5f28af9.js"
+  },
+  {
+    "revision": "b809ae04c9dbafc465cef6210005e1a0",
+    "url": "/devima_react-test/static/media/BrandonText-Black.b809ae04.eot"
+  },
+  {
+    "revision": "e3cb7c14623ec6cbc3f99aeaccba9fcb",
+    "url": "/devima_react-test/static/media/BrandonText-Black.e3cb7c14.woff"
+  },
+  {
+    "revision": "f81ecad605e722ab9facc6260509fc69",
+    "url": "/devima_react-test/static/media/BrandonText-Black.f81ecad6.ttf"
+  },
+  {
+    "revision": "133ffcc4bcb4079feb900cc5f23cf4bd",
+    "url": "/devima_react-test/static/media/BrandonText-BlackItalic.133ffcc4.woff"
+  },
+  {
+    "revision": "446c85273b79005dac56595130229904",
+    "url": "/devima_react-test/static/media/BrandonText-BlackItalic.446c8527.eot"
+  },
+  {
+    "revision": "a50f916ea580fb3fe2d7acf7c42ad0b2",
+    "url": "/devima_react-test/static/media/BrandonText-BlackItalic.a50f916e.ttf"
+  },
+  {
+    "revision": "5be1efdeb855a3ffa40474f34e9e1f85",
+    "url": "/devima_react-test/static/media/BrandonText-Bold.5be1efde.woff"
+  },
+  {
+    "revision": "6d5918ced316b1d65f4d7311acf5bf88",
+    "url": "/devima_react-test/static/media/BrandonText-Bold.6d5918ce.ttf"
+  },
+  {
+    "revision": "9c5348003d8ef0d78ba4b2fe276e1d79",
+    "url": "/devima_react-test/static/media/BrandonText-Bold.9c534800.eot"
+  },
+  {
+    "revision": "8bd149aa1705794c7f4f03567bd4cdc1",
+    "url": "/devima_react-test/static/media/BrandonText-BoldItalic.8bd149aa.ttf"
+  },
+  {
+    "revision": "eed44c54bd9bac82f551e60a95c259e3",
+    "url": "/devima_react-test/static/media/BrandonText-BoldItalic.eed44c54.woff"
+  },
+  {
+    "revision": "fa56636de7d2bc7d4aaaee43411064eb",
+    "url": "/devima_react-test/static/media/BrandonText-BoldItalic.fa56636d.eot"
+  },
+  {
+    "revision": "842890aec56568b5bd078defcef0dc7e",
+    "url": "/devima_react-test/static/media/BrandonText-Light.842890ae.ttf"
+  },
+  {
+    "revision": "dfde6e155619991337bbc8bfc1f6d21c",
+    "url": "/devima_react-test/static/media/BrandonText-Light.dfde6e15.woff"
+  },
+  {
+    "revision": "e025c2d5e71b21c37e934b9b9e97f58b",
+    "url": "/devima_react-test/static/media/BrandonText-Light.e025c2d5.eot"
+  },
+  {
+    "revision": "1f836b2cb6748539a9a6da8c80006564",
+    "url": "/devima_react-test/static/media/BrandonText-LightItalic.1f836b2c.ttf"
+  },
+  {
+    "revision": "ba49fceff8acea213af4229ba54730e7",
+    "url": "/devima_react-test/static/media/BrandonText-LightItalic.ba49fcef.woff"
+  },
+  {
+    "revision": "c24665f08eafd877d128487a4feb052f",
+    "url": "/devima_react-test/static/media/BrandonText-LightItalic.c24665f0.eot"
+  },
+  {
+    "revision": "7cff9eec7df0804e0d69f48ba3ccc6e8",
+    "url": "/devima_react-test/static/media/BrandonText-Medium.7cff9eec.ttf"
+  },
+  {
+    "revision": "819371bd60f342e15b2066bec27643f0",
+    "url": "/devima_react-test/static/media/BrandonText-Medium.819371bd.woff"
+  },
+  {
+    "revision": "cbff7dcc464ae6dc493a54eb9d8de0c6",
+    "url": "/devima_react-test/static/media/BrandonText-Medium.cbff7dcc.eot"
+  },
+  {
+    "revision": "69725844ccd99e92902de2dc809f0caa",
+    "url": "/devima_react-test/static/media/BrandonText-MediumItalic.69725844.woff"
+  },
+  {
+    "revision": "e241dd6f89885e44e7195d361d9425eb",
+    "url": "/devima_react-test/static/media/BrandonText-MediumItalic.e241dd6f.ttf"
+  },
+  {
+    "revision": "f55e783d8e2f872f0b57c771e8c9d1f5",
+    "url": "/devima_react-test/static/media/BrandonText-MediumItalic.f55e783d.eot"
+  },
+  {
+    "revision": "632f6a765ae38914c939fbb1e39ae89c",
+    "url": "/devima_react-test/static/media/BrandonText-Regular.632f6a76.eot"
+  },
+  {
+    "revision": "709723e4716e3e2b5c98603b9425da4a",
+    "url": "/devima_react-test/static/media/BrandonText-Regular.709723e4.ttf"
+  },
+  {
+    "revision": "fadd85918b8eb65cae21dfac32734256",
+    "url": "/devima_react-test/static/media/BrandonText-Regular.fadd8591.woff"
+  },
+  {
+    "revision": "60fad9def5bd3ce4b70dc595f427f75b",
+    "url": "/devima_react-test/static/media/BrandonText-RegularItalic.60fad9de.ttf"
+  },
+  {
+    "revision": "9f38937c43231ab4e6d65d7fd2f6436f",
+    "url": "/devima_react-test/static/media/BrandonText-RegularItalic.9f38937c.woff"
+  },
+  {
+    "revision": "f38c590a49c2d48289a6c54130247de9",
+    "url": "/devima_react-test/static/media/BrandonText-RegularItalic.f38c590a.eot"
+  },
+  {
+    "revision": "2ccd15de7f21d05da3506e04a98cbeaf",
+    "url": "/devima_react-test/static/media/BrandonText-Thin.2ccd15de.ttf"
+  },
+  {
+    "revision": "43a2d3917787e1034dd7ce292490de1c",
+    "url": "/devima_react-test/static/media/BrandonText-Thin.43a2d391.eot"
+  },
+  {
+    "revision": "bc2fc9ab9556fca8dee3026af182f18e",
+    "url": "/devima_react-test/static/media/BrandonText-Thin.bc2fc9ab.woff"
+  },
+  {
+    "revision": "300d612e9acf3d4649826c3bb977c860",
+    "url": "/devima_react-test/static/media/BrandonText-ThinItalic.300d612e.woff"
+  },
+  {
+    "revision": "db58e95785821ec3ae4013e4b4d4d915",
+    "url": "/devima_react-test/static/media/BrandonText-ThinItalic.db58e957.ttf"
+  },
+  {
+    "revision": "f2513d9f662a19c37271cdb5b544a079",
+    "url": "/devima_react-test/static/media/BrandonText-ThinItalic.f2513d9f.eot"
+  },
+  {
+    "revision": "dc79f87bd354bce63fbb34777a11bf0a",
+    "url": "/devima_react-test/static/media/desktop.dc79f87b.png"
+  },
+  {
+    "revision": "41defda7d181f1ac381f9aa2df913867",
+    "url": "/devima_react-test/static/media/screens-challenge.41defda7.png"
+  },
+  {
+    "revision": "c6b73c3956b510fbd028bdaa5eb2c80e",
+    "url": "/devima_react-test/static/media/services-screen.c6b73c39.png"
+  },
+  {
+    "revision": "f1a79c60217930f143a72bddee8fff30",
+    "url": "/devima_react-test/static/media/solutions-screen.f1a79c60.png"
+  }
+]);
